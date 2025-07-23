c3fc6744f353052af17953688e0cdfaeb521ab0b
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2025-07-23 12:23:43 +0530

    'learnt about query params, headers , cookies and caching'

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-  return new Response("Profile Data");
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
+
+  const cookies = request.cookies.get("theme");
+  console.log(cookies);
+  return Response.json("Profile Data", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
---
+++ route-handlers-demo/src/app/time/categories/route.ts
@@ -0,0 +1,9 @@
+export const dynamic = "force-static";
+export async function GET() {
+  const categories = [
+    { id: 1, name: "Electronics" },
+    { id: 2, name: "Books" },
+    { id: 3, name: "Footwear" },
+  ];
+  return Response.json(categories);
+}
